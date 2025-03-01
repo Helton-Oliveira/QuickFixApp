f04267e062ffe23be05cbf73e02f4cc5af2caf30
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-02-28 22:30:01 -0300

    feat: customer registration use case test

---
+++ src/test/kotlin/com/digisphere/QuickFix/services/ClientUseCaseTest.kt
@@ -0,0 +1,27 @@
+package com.digisphere.QuickFix.services
+
+import com.digisphere.QuickFix.client.DTOs.ClientForm
+import com.digisphere.QuickFix.client.domain.Role
+import com.digisphere.QuickFix.infra.connection.ConnectionImpl
+import com.digisphere.QuickFix.client.useCases.ClientRegisterImpl
+import com.digisphere.QuickFix.client.infra.repository.ClientRepository
+import com.digisphere.QuickFix.client.infra.repository.ClientRepositoryInMemory
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class ClientUseCaseTest {
+
+    private val connection = ConnectionImpl.createTestDatabase()
+    private val repository = ClientRepository(connection)
+    private val repositoryFake = ClientRepositoryInMemory()
+    private val clientForm = ClientForm(name = "Tony Stark", email = "Tony@avengers.com", cpf = "1234567890", password = "senha1234", role = Role.CLIENT)
+    private val register = ClientRegisterImpl(repository)
+
+    @Test
+    fun `deve criar um usuario`() {
+        val client = register.execute(clientForm)
+        assertThat(client.name).isEqualTo("Tony Stark")
+        assertThat(client.email).isEqualTo("Tony@avengers.com")
+        assertThat(client.cpf).isEqualTo("1234567890")
+    }
+}
