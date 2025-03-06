615ef37918bb384549647f630d248c82a2048123
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-03-05 22:59:05 -0300

    refactor: add extension function for client conversion and rename methods from toModel to toViewModel

--- src/main/kotlin/com/digisphere/QuickFix/client/infra/entities/util/ClientEntityExtension.kt
+++ src/main/kotlin/com/digisphere/QuickFix/client/infra/entities/util/ClientEntityExtension.kt
@@ -2,13 +2,34 @@ package com.digisphere.QuickFix.client.infra.entities.util
 
 import com.digisphere.QuickFix.client.DTOs.ClientForm
 import com.digisphere.QuickFix.client.DTOs.ClientView
+import com.digisphere.QuickFix.client.domain.Client
 import com.digisphere.QuickFix.client.infra.entities.ClientEntity
 
-fun ClientEntity.toModel(): ClientView {
+fun ClientEntity.toViewModel(): ClientView {
     return ClientView(
         name = this.name,
         email = this.email,
         cpf = this.cpf,
+        role = this.role
+    )
+}
+
+fun ClientEntity.toDomainModel(): Client {
+    return Client(
+        name = this.name,
+        email = this.email,
+        cpf = this.cpf,
+        password = this.password,
+        role = this.role
+    )
+}
+
+fun Client.toEntity(): ClientEntity {
+    return ClientEntity(
+        cpf = this.cpf,
+        name = this.name,
+        email = this.email,
+        password = this.password,
         role = this.role
     )
 }
--- src/main/kotlin/com/digisphere/QuickFix/client/useCases/ClientRegisterImpl.kt
+++ src/main/kotlin/com/digisphere/QuickFix/client/useCases/ClientRegisterImpl.kt
@@ -4,13 +4,13 @@ import com.digisphere.QuickFix.client.DTOs.ClientForm
 import com.digisphere.QuickFix.client.DTOs.ClientView
 import com.digisphere.QuickFix.infra.repository.GenericRepository
 import com.digisphere.QuickFix.client.infra.entities.util.toEntity
-import com.digisphere.QuickFix.client.infra.entities.util.toModel
+import com.digisphere.QuickFix.client.infra.entities.util.toViewModel
 
 class ClientRegisterImpl(private val repository: GenericRepository)
     : ClientRegister {
 
     override fun execute(data: ClientForm): ClientView {
         val entity = repository.persist(data.toEntity())
-        return entity.toModel()
+        return entity.toViewModel()
     }
 }
